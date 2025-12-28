7db5388e657ec78bc31c8b2c3a5f35157a4cb82e
Takadono-Shirayuki <Thanh.BT225669@sis.hust.edu.vn>
2025-12-28 21:21:33 +0700

    1

--- handler/utils.hpp
+++ handler/utils.hpp
@@ -2,5 +2,3 @@
 
 ssize_t find_device_info_by_id(vector<DeviceInfo> &list, string id);
 ssize_t find_device_by_id(vector<Device*> &devices, string id);
-bool get_local_ipv4(string &out_ip);
-int connect_to(const string &ip, uint16_t port, int timeout_ms = 200);
