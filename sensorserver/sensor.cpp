d202e1a2d8a9e4abfc7ca24cb76173e109d4a3b1
Takadono-Shirayuki <Thanh.BT225669@sis.hust.edu.vn>
2025-12-29 10:53:43 +0700

    Create sensor.cpp

---
+++ sensorserver/sensor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+// Include sensor class header for the test binary
+#include "sensor.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc < 5) {
+        std::cout << "Usage: test_sensor [PORT] [SENSOR_ID] [NAME] [PASSWORD]" << std::endl;
+        return 1;
+    }
+    int port = atoi(argv[1]);
+    std::string id = argv[2];
+    std::string name = argv[3];
+    std::string pass = argv[4];
+
+    Sensor sensor((uint16_t)port, id, name, pass);
+    sensor.run();
+    return 0;
+}
